c8c15b047b24687a5e9032e6356f85ac2bd261de
rinicro <ruben.ruper@gmail.com>
2020-02-18 17:55:19 +0100

    Práctica 0 terminada

--- Práctica 0/Práctica0.py
+++ Práctica 0/Práctica0.py
@@ -7,6 +7,7 @@ Rubén Ruperto Díaz y Rafael Herrera Troca
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as scpint
 
 # Función con la que trabajaremos f(x) = sin(x) + 1
 def f(x):
@@ -41,25 +42,41 @@ plt.savefig("func.png")
 plt.show()
 
 # Calculamos en un bucle el tiempo de ejecución para cada una de las opciones
-# haciéndolo cada vez con más precisión
+# y el error cometido, haciéndolo cada vez con más precisión
 times = ([],[])
+error = ([],[])
+res = scpint.quad(f,0,2*np.pi)[0]
 for n in np.linspace(1000,1000000,51):
     time1 = time.process_time()
-    integra_mc_it(f,0,2*np.pi, int(n))
+    res1 = integra_mc_it(f,0,2*np.pi, int(n))
     time2 = time.process_time()
-    integra_mc_vc(f,0,2*np.pi, int(n))
+    res2 = integra_mc_vc(f,0,2*np.pi, int(n))
     time3 = time.process_time()
     times[0].append(time2 - time1)
     times[1].append(time3 - time2)
+    error[0].append(abs(res - res1))
+    error[1].append(abs(res - res2))
     
 # Representamos el tiempo que tardan las dos funciones en ejecutarse según la
 # precisión que utilicemos
 plt.figure(figsize=(10,10))
 plt.plot(np.linspace(1000,1000000,51), times[0], 'r', label="Tiempo utilizando un bucle")
 plt.plot(np.linspace(1000,1000000,51), times[1], 'g', label="Tiempo utilizando un vector")
-plt.title("Tiempo de ejecución de \textit{integra_mc}")
-plt.xlabel("$Número de puntos$")
-plt.ylabel("$Tiempo de ejecución$")
+plt.title("Tiempo de ejecución de 'integra_mc'")
+plt.xlabel("Número de puntos")
+plt.ylabel("Tiempo de ejecución")
 plt.legend(loc="upper left")
 plt.savefig("times.png")
+plt.show()
+
+# Representamos el error cometido por las dos funciones según la precisión que
+# utilicemos
+plt.figure(figsize=(10,10))
+plt.plot(np.linspace(1000,1000000,51), error[0], 'r', label="Error utilizando un bucle")
+plt.plot(np.linspace(1000,1000000,51), error[1], 'g', label="Error utilizando un vector")
+plt.title("Error en el resultado de 'integra_mc'")
+plt.xlabel("Número de puntos")
+plt.ylabel("Error cometido")
+plt.legend(loc="upper right")
+plt.savefig("error.png")
 plt.show()
