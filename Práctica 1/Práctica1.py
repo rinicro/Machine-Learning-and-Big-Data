995e4f40a2a0774daaacc1b7229ce85bacabd37a
errachete <raherrer@ucm.es>
2020-02-25 02:23:02 +0100

    Axis3D borrado por error añadido

--- Práctica 1/Práctica1.py
+++ Práctica 1/Práctica1.py
@@ -5,6 +5,7 @@ Rubén Ruperto Díaz y Rafael Herrera Troca
 '''
 
 from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
